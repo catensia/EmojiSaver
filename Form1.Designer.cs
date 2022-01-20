859a436022d2c1e9f61fb74ba37fbb84d517aff5
catensia <catenarywkd1@naver.com>
2022-01-20 15:28:39 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace EmojiSaver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.프로그램종료ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.창활성화ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.프로그램종료ToolStripMenuItem,
+            this.창활성화ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(151, 48);
+            // 
+            // 프로그램종료ToolStripMenuItem
+            // 
+            this.프로그램종료ToolStripMenuItem.Name = "프로그램종료ToolStripMenuItem";
+            this.프로그램종료ToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.프로그램종료ToolStripMenuItem.Text = "프로그램 종료";
+            this.프로그램종료ToolStripMenuItem.Click += new System.EventHandler(this.프로그램종료ToolStripMenuItem_Click);
+            // 
+            // 창활성화ToolStripMenuItem
+            // 
+            this.창활성화ToolStripMenuItem.Name = "창활성화ToolStripMenuItem";
+            this.창활성화ToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.창활성화ToolStripMenuItem.Text = "창 활성화";
+            this.창활성화ToolStripMenuItem.Click += new System.EventHandler(this.창활성화ToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(44, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(221, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 368);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDoubleClick);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 프로그램종료ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 창활성화ToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using Hotkeys;
+
+namespace EmojiSaver
+{
+    public partial class Form1 : Form
+    {
+
+        // for console debug
+        static class NativeMethods
+        {
+            [DllImport("kernel32.dll", SetLastError = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            public static extern bool AllocConsole();
+        }
+
+        private IDataObject previousClipboard;
+        private void setPreviousClipboard(IDataObject currentClipboardObj)
+        {
+            previousClipboard = currentClipboardObj;
+        }
+        private IDataObject getPreviousClipboard()
+        {
+            return previousClipboard;
+        }
+
+        private void restoreCurrentClipboard(ref IDataObject currentClipboardObj)
+        {
+            currentClipboardObj = previousClipboard;
+        }
+
+        private Hotkeys.GlobalHotkey ghk;
+
+        private Hotkeys.GlobalHotkey[] ghk2;
+        public Form1()
+        {
+            InitializeComponent();
+
+            // for console debugging
+            NativeMethods.AllocConsole();
+
+            //initialize global hotkeys 
+            ghk = new Hotkeys.GlobalHotkey(Constants.ALT + Constants.SHIFT, Keys.O, this);
+            ghk2 = new Hotkeys.GlobalHotkey[10];
+            for(int i = 0; i < ghk2.Length; i++)
+            {
+                ghk2[i] = new Hotkeys.GlobalHotkey(Constants.CTRL, Keys.D1 + i, this);
+            }
+        }
+
+        private void WriteLine(string text)
+        {
+            textBox1.Text += text + Environment.NewLine;
+        }
+
+        private void HandleHotkey(ref Message m)
+        {
+            Console.WriteLine(m.LParam.ToString("X"));
+            int HotkeyIdx = (m.LParam.ToInt32() & 0x0F0000) >> 16;
+            Console.WriteLine("CTRL + " + HotkeyIdx + " is pressed!");
+
+            /*
+             * Maybe should restore previous clipboard contents??
+             */
+
+            /* save current clipboard contents to previousClipboard variable */
+            setPreviousClipboard(Clipboard.GetDataObject());
+
+            /* clear current clipboard */
+            Clipboard.Clear();
+
+            /* CTRL + C and get current selected text */
+            Thread.Sleep(100);
+            SendKeys.SendWait("^c");
+            Thread.Sleep(100);
+
+            if (Clipboard.ContainsText())
+            {
+                var selectedText = Clipboard.GetText();
+                Console.WriteLine(selectedText);
+                Console.WriteLine(Clipboard.GetDataObject().GetData(Text));
+
+            }
+            else
+            {
+                Console.WriteLine("Nothing selected");
+            }
+
+            /* restore current clipboard with previous content */
+            Clipboard.SetDataObject(getPreviousClipboard());
+            
+        }
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == Hotkeys.Constants.WM_HOTKEY_MSG_ID)
+            {
+                HandleHotkey(ref m);
+            }
+            base.WndProc(ref m);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            WriteLine("Trying to register SHIFT+ALT+O");
+          
+            if (ghk.Register()) WriteLine("Hotkey registered.");
+            else WriteLine("Hotkey failed to register");
+            
+            for(int i = 0; i < ghk2.Length; i++)
+            {
+                if (ghk2[i].Register()) Console.WriteLine("Hotkey CTRL + " + (i + 1) + " registered");
+                else Console.WriteLine("Hotkey CTRL + " + (i + 1) + " failed to register");
+            }
+            
+        }
+
+
+        private void Use_Notify()
+        {
+            notifyIcon1.ContextMenuStrip = contextMenuStrip1;
+            notifyIcon1.BalloonTipText = "Test입니다";
+            notifyIcon1.BalloonTipTitle = "Form1";
+            notifyIcon1.ShowBalloonTip(1);
+        }
+
+
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if(FormWindowState.Minimized == WindowState)
+            {
+                Use_Notify();
+                notifyIcon1.Visible = true;
+                this.Hide();
+            }
+            else if(FormWindowState.Normal == this.WindowState)
+            {
+                notifyIcon1.Visible = false;
+                this.ShowInTaskbar = true;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            this.Hide();
+            Use_Notify();
+            notifyIcon1.Visible = true;
+        }
+
+        private void Form1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                this.Visible = true;
+                this.ShowInTaskbar = true;
+                this.WindowState = FormWindowState.Normal;
+                notifyIcon1.Visible = false;
+            }
+        }
+
+        private void 프로그램종료ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            if (!ghk.Unregiser()) MessageBox.Show("Hotkey failed to unregister!");
+            notifyIcon1.Dispose();
+            Application.ExitThread();
+        }
+
+        private void 창활성화ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Visible = true;
+            this.ShowInTaskbar = true;
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon1.Visible = false;
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Visible = true;
+            this.ShowInTaskbar = true;
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon1.Visible = false;
+        }
+
+    }
+}
